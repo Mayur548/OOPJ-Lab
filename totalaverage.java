f53b54f5aa6a33d5e524234b3ebdb92058244cbc
Mayur548 <parvathammayur@gmail.com>
2026-02-04 11:02:11 +0530

    totalaverage.java

--- totalaverage.java
+++ totalaverage.java
@@ -3,19 +3,19 @@ class totalaverage
 	static int a,b;
 	static int total;
 	static double average;
-	calculate()
+	static void calculate()
 	{
 		 total = a+b;
 		 average = total/2.0;
 	}
 	public static void main(String[] args)
 	{
-		a=Integer.parse Int(args[0]);
-		b=Integer.parse Int(args[1]);
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
 		calculate();
-}
 		System.out.println("first number:="+a);
 		System.out.println("second number:="+b);
-		System.out.println("total:");
-		System.out.println("average:");
+		System.out.println("total:"+total);
+		System.out.println("average:"+average);
+}
 }
